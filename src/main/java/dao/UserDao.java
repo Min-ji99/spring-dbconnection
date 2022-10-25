7233273426e7803ee8a386dfc2364882ef5db7ea
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-25 11:58:02 +0900

    try/catch/finally 적용

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -13,79 +13,113 @@ public class UserDao {
     public UserDao(ConnectonMaker connectonMaker){this.connectonMaker=connectonMaker;}
 
     public void deleteAll(){
+        Connection c=null;
+        PreparedStatement ps=null;
         try{
-            Connection c = connectonMaker.makeConnection();
+            c = connectonMaker.makeConnection();
 
-            PreparedStatement ps=c.prepareStatement("delete from users");
+            ps=c.prepareStatement("delete from users");
             ps.executeUpdate();
-
-            ps.close();
-            c.close();
         }catch(SQLException e){
             e.printStackTrace();
+        }finally{
+            if(ps!=null){
+                try{
+                    ps.close();
+                }catch (SQLException e){}
+            }
+            if(c!=null){
+                try{
+                    c.close();
+                }catch(SQLException e){}
+            }
         }
     }
     public int getCount(){
+        Connection c=null;
+        PreparedStatement ps=null;
+        ResultSet rs=null;
+        int count=0;
         try{
-            Connection c= connectonMaker.makeConnection();
-            PreparedStatement ps=c.prepareStatement("select count(*) from users");
-            ResultSet result=ps.executeQuery();
-            result.next();
+            c= connectonMaker.makeConnection();
+            ps=c.prepareStatement("select count(*) from users");
+            rs=ps.executeQuery();
+            rs.next();
 
-            int count=result.getInt(1);
-
-            result.close();
-            ps.close();
-            c.close();
+            count=rs.getInt(1);
 
             return count;
         }catch(SQLException e){
             e.printStackTrace();
+        }finally{
+            if(ps!=null){
+                try{
+                    ps.close();
+                }catch (SQLException e){}
+            }
+            if(c!=null){
+                try{
+                    c.close();
+                }catch(SQLException e){}
+            }
+            if(rs!=null){
+                try{
+                    rs.close();
+                }catch (SQLException e){}
+            }
         }
-        return 0;
+        return count;
     }
     public void add(User user) {
+        Connection c=null;
+        PreparedStatement ps=null;
         try {
             // DB접속 (ex sql workbeanch실행)
-            Connection c = connectonMaker.makeConnection();
+            c = connectonMaker.makeConnection();
 
             // Query문 작성
-            PreparedStatement pstmt = c.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?);");
-            pstmt.setString(1, user.getId());
-            pstmt.setString(2, user.getName());
-            pstmt.setString(3, user.getPassword());
+            ps = c.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?);");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
 
             // Query문 실행
-            pstmt.executeUpdate();
-
-            pstmt.close();
-            c.close();
+            ps.executeUpdate();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }finally{
+            if(ps!=null){
+                try{
+                    ps.close();
+                }catch (SQLException e){}
+            }
+            if(c!=null){
+                try{
+                    c.close();
+                }catch(SQLException e){}
+            }
         }
     }
 
     public User findById(String id) {
         User user=null;
+        Connection c=null;
+        PreparedStatement ps=null;
+        ResultSet rs=null;
         try {
             // DB접속 (ex sql workbeanch실행)
-            Connection c = connectonMaker.makeConnection();
+            c = connectonMaker.makeConnection();
 
             // Query문 작성
-            PreparedStatement pstmt = c.prepareStatement("SELECT * FROM users WHERE id = ?");
-            pstmt.setString(1, id);
+            ps = c.prepareStatement("SELECT * FROM users WHERE id = ?");
+            ps.setString(1, id);
 
             // Query문 실행
-            ResultSet rs = pstmt.executeQuery();
+            rs = ps.executeQuery();
             rs.next();
-
             user = new User(rs.getString("id"), rs.getString("name"),
                     rs.getString("password"));
-
-            rs.close();
-            pstmt.close();
-            c.close();
             if(user==null){
                 throw new EmptyResultDataAccessException(1);
             }
@@ -93,6 +127,22 @@ public class UserDao {
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }finally{
+            if(ps!=null){
+                try{
+                    ps.close();
+                }catch (SQLException e){}
+            }
+            if(c!=null){
+                try{
+                    c.close();
+                }catch(SQLException e){}
+            }
+            if(rs!=null){
+                try{
+                    rs.close();
+                }catch (SQLException e){}
+            }
         }
     }
 }
