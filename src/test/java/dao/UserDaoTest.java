f1d6a4cd1e8f31c837434989e1aededbd3a39929
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-25 11:21:37 +0900

    Spring 도입

--- src/test/java/dao/UserDaoTest.java
+++ src/test/java/dao/UserDaoTest.java
@@ -2,13 +2,24 @@ package dao;
 
 import domain.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
+    @Autowired
+    ApplicationContext context;
+
+    UserDao userDao;
     @Test
     public void addAndGet(){
-        UserDao userDao=new UserDao();
+        userDao=context.getBean("awsUserDao", UserDao.class);
         userDao.add(new User("4", "nana", "1234"));
         User user=userDao.findById("4");
         assertEquals("nana", user.getName());
